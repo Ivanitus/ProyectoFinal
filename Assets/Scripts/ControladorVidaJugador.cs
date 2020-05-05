90a1c3278ca9dfe8693fd0275c427f53edf8dca1
Ivanitus <icamps555@gmail.com>
2020-05-05 21:15:57 +0200

    Deteccion daño y vidas
    Implementación de vidas maximas y actuales
    Ahora el jugador recibe daño al tocar los pinchos.
    En caso de perder todas las vidas, el jugador desaparece.
    Añadido corazones que indican las vidas, pendientes de ser funcionales (WIP)
    Solución de error de escalado de los corazones dependiendo de la resolución de ejecución del juego.

---
+++ Assets/Scripts/ControladorVidaJugador.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Clase para controlar la vida del jugador
+public class ControladorVidaJugador : MonoBehaviour {
+
+    public static ControladorVidaJugador instancia; // Singleton
+
+    public int vidasActuales, vidasMaximas;
+
+    // Metodo sobreescrito de Unity que se ejecuta justo antes de Start()
+    private void Awake() {
+
+        instancia = this;
+
+    }
+
+    // Start is called before the first frame update
+    void Start() {
+
+        vidasActuales = vidasMaximas;
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+    }
+
+    public void hacerDano() {
+
+        vidasActuales--;
+
+        if(vidasActuales <= 0) {
+
+            gameObject.SetActive(false); //gameObject hace referencia al objeto al que está anclado el script
+
+        }
+
+    }
+
+}
