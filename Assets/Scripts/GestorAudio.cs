df0b1fbf3b7d701e4fe3e5ad12b7f48a452c42af
Ivanitus <icamps555@gmail.com>
2020-05-12 17:16:31 +0200

    Añadido musica de fondo y mezclador
    Añadida la música de fondo.
    Añadido mezclador de sonido.
    Añadido tema de victoria al final del nivel, pero pendiente de ser implementado más adelante.

--- Assets/Scripts/GestorAudio.cs
+++ Assets/Scripts/GestorAudio.cs
@@ -9,6 +9,10 @@ public class GestorAudio : MonoBehaviour {
 
     public AudioSource[] efectosSonido;
 
+    public AudioSource musicaFondo;
+
+    public AudioSource musicaFinalNivel;
+
     private void Awake() {
 
         instancia = this;
@@ -32,6 +36,8 @@ public class GestorAudio : MonoBehaviour {
     public void reproducirSFX(int sonidoAReproducir) {
 
         efectosSonido[sonidoAReproducir].Stop(); // En el caso de que tengan que sonar varias veces el mismo sonido a la vez, necesitamos pararlos antes de volver a reproducirlo
+
+        efectosSonido[sonidoAReproducir].pitch = Random.Range(.85f, 1.15f); // Hago que el pitch cambie de forma aleatoria para que los efectos de sonido suenen distintos cada vez
 
         efectosSonido[sonidoAReproducir].Play();
 
