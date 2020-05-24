906f5d1349b04de44983db91ed95e589c74add65
Ivanitus <icamps555@gmail.com>
2020-05-24 05:23:58 +0200

    Desarrollo basico del boss
    Se crean los diversos estados del boss final.
    Se empiezan a cambiar entre distintos estados del boss final.
    Uso y conexion de animaciones del boss.
    Añadida bala.
    Añadidad animacion a la bala.
    Ahora la bala hace daño al personaje.
    La bala se mueve a izquierda o derecha dependiendo su localScale en la coordenada X

--- Assets/Scripts/ControladorBossTanque.cs
+++ Assets/Scripts/ControladorBossTanque.cs
@@ -2,7 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+// Clase para controlar el Boss final (tanque)
 public class ControladorBossTanque : MonoBehaviour {
+
+    public enum estadosBoss { DISPARANDO, DANADO, MOVIENDO }; // estados del boss (Disparando=quieto y/o disparando, DANADO=recibe daño, MOVIENDO=se mueve)
+    public estadosBoss estadoActual;
 
     // Variables jefe
     [Header("Jefe")]
@@ -28,14 +32,105 @@ public class ControladorBossTanque : MonoBehaviour {
     private float contadorDano;
 
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start() {
+
+        estadoActual = estadosBoss.DISPARANDO;
+
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
         
+        switch(estadoActual) {
+
+            case estadosBoss.DISPARANDO:
+                break;
+
+            case estadosBoss.DANADO:
+
+                if (contadorDano > 0) {
+
+                    contadorDano -= Time.deltaTime;
+
+                    if (contadorDano <= 0) {
+
+                        estadoActual = estadosBoss.MOVIENDO;
+
+                    }
+
+                }
+
+                break;
+
+            case estadosBoss.MOVIENDO:
+
+                if (moverDerecha) {
+
+                    boss.position += new Vector3(velocidadMovimiento * Time.deltaTime, 0f, 0f);
+
+                    if (boss.position.x > puntoDerecha.position.x) {
+
+                        boss.localScale = Vector3.one; // Lo oriento hacia la izquierda con localscale para cambiar tambien el punto de disparo
+
+                        moverDerecha = false;
+
+                        finalizarMovimiento();
+
+                    }
+
+                } else {
+
+                    boss.position -= new Vector3(velocidadMovimiento * Time.deltaTime, 0f, 0f);
+
+                    if (boss.position.x < puntoIzquierda.position.x) {
+
+                        boss.localScale = new Vector3(-1f, 1f, 1f); // Lo oriento hacia la derecha con localscale para cambiar tambien el punto de disparo
+
+                        moverDerecha = true;
+
+                        finalizarMovimiento();
+
+                    }
+
+                }
+
+                break;
+
+            default:
+                break;
+
+        }
+
+        #if UNITY_EDITOR // Cualquier codigo debajo de esta linea solo se ejecutara si estamos en el editor de Unity
+
+        if (Input.GetKeyDown(KeyCode.H)) {
+
+            recibirGolpe();
+
+        }
+
+        #endif
+
     }
+
+    public void recibirGolpe() {
+
+        estadoActual = estadosBoss.DANADO;
+
+        contadorDano = tiempoDano;
+
+        animador.SetTrigger("Danado");
+
+    }
+
+    private void finalizarMovimiento() {
+
+        estadoActual = estadosBoss.DISPARANDO;
+
+        contadorDisparo = tiempoEntreDisparos;
+
+        animador.SetTrigger("PararMovimiento");
+
+    }
+
 }
